ea3de66370f30ce3bfe21e3bad2a27d7da9a6c25
kapibaralower <kodman2005@gmail.com>
2023-09-16 16:41:49 +0300

    Code for module 9 in raport

--- code/venv/module_9_outlines.py
+++ code/venv/module_9_outlines.py
@@ -13,7 +13,9 @@ if __name__ == "__main__":
 
     grad_x = cv.Sobel(gray, ddepth, 1, 0, ksize=3, scale=scale, delta=delta)
 
-    print("ESC - exit\nf - Sharr gradient of y\ns - Sobel gradient of y")
+    abs_grad_x = cv.convertScaleAbs(grad_x)
+
+    print("ESC - exit\nf - Sharr gradient of y\ns - Sobel gradient of y\nl - Laplacian outlines")
 
     while 1:
         cv.imshow("Original image", image)
@@ -22,14 +24,24 @@ if __name__ == "__main__":
 
         if k == 27:
             break
+
         elif chr(k) == 'f':
             grad_y = cv.Scharr(gray,ddepth,0,1)
+            abs_grad_y = cv.convertScaleAbs(grad_y)
+
+            grad = cv.addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0)
+
+            cv.imshow("Sharr and Sobel outlines", grad)
+
         elif chr(k) == 's':
             grad_y = cv.Sobel(gray, ddepth, 0, 1, ksize=3, scale=scale, delta=delta)
+            abs_grad_y = cv.convertScaleAbs(grad_y)
 
-        abs_grad_x = cv.convertScaleAbs(grad_x)
-        abs_grad_y = cv.convertScaleAbs(grad_y)
+            grad = cv.addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0)
 
-        grad = cv.addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0)
+            cv.imshow("Sobel outlines", grad)
 
-        cv.imshow("Outlines", grad)
+        elif chr(k) == 'l':
+            laplacian = cv.Laplacian(gray, ddepth=ddepth, ksize=3)
+            abs_laplacian = cv.convertScaleAbs(laplacian)
+            cv.imshow("Laplacian outlines", abs_laplacian)
